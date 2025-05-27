7d64f9bdde938a37eb3996bde4672533ad8c7c9b
KArtem <k6art5@gmail.com>
2025-05-27 12:58:33 +0700

    comleted work!

--- assets/js/preloader_controller.js
+++ assets/js/preloader_controller.js
@@ -2,11 +2,11 @@ function preloaderInit() {
   const body = document.querySelector('body');
   const preloader = document.getElementById('preloader');
 
-  const idTimer = setTimeout(() => {
+  const idTimerLoaded = setTimeout(() => {
     body.classList.add('loaded');
     preloader.classList.add('preloader--unvisible')
 
-    clearTimeout(idTimer)
+    clearTimeout(idTimerLoaded)
     document.removeEventListener('DOMContentLoaded', preloaderInit);
   }, Number(preloader.dataset?.timeout) || 1000);
 }
